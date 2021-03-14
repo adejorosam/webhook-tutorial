c9bf4fa557d2483cba15ef3f45efa7d520cca0ee
Samson Adejoro <sadejoro@sbsc.com>
2021-03-14 11:22:32 +0100

    ch: Fixed issues resulting fro missing package

--- app/Handler/ProcessWebhook.php
+++ app/Handler/ProcessWebhook.php
@@ -1,5 +1,6 @@
 <?php
 namespace App\Handler;
+use Log;
 use \Spatie\WebhookClient\ProcessWebhookJob;
 //The class extends "ProcessWebhookJob" class as that is the class 
 //that will handle the job of processing our webhook before we have 
@@ -11,6 +12,7 @@ class ProcessWebhook extends ProcessWebhookJob
 public function handle(){
        $data = json_decode($this->webhookCall, true);
        //Do something with the event
+       Log::info($data);
        http_response_code(200); //Acknowledge you received the response
     }
 }
